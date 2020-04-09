911d4be09082b852be72631dfed2d86528799761
xisco <xisco.capllonch.amer@gmail.com>
2020-04-09 11:29:28 +0200

    add initialize options func

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -14,37 +13,93 @@ type box struct {
 }
 
 type sk struct {
-	boxes [81]box
-}
-
-func main() {
-	sk := createSk("000000000035070840097302510003904100060000090009503700051608920026090450000000000")
-	fmt.Println("sk:", sk.boxes[10])
-
+	boxes   [81]box
+	options struct {
+		rows    map[int][]int
+		cols    map[int][]int
+		squares map[int][]int
+	}
 }
 
 func createSk(input string) sk {
-	var boxes [81]box
+	var boxesInput [81]int
 
 	for idx, value := range strings.Split(input, "") {
-		v, _ := strconv.Atoi(value)
+		intV, _ := strconv.Atoi(value)
+		boxesInput[idx] = intV
+	}
+
+	newSk := sk{}
+
+	newSk.initBoxes(boxesInput)
+	newSk.initOptions()
+
+	return newSk
+}
+
+func (s *sk) initBoxes(input [81]int) {
+	var boxes [81]box
+
+	for idx, value := range input {
 		boxes[idx] = box{
-			value: v,
-			rowIdx: func () int {
-				return (idx%9-idx)/-9
+			value: value,
+			rowIdx: func() int {
+				return (idx%9 - idx) / -9
 			}(),
-			colIdx: func () int {
-				return idx%9
+			colIdx: func() int {
+				return idx % 9
 			}(),
-			squareIdx: func () int {
-				col := idx%9
-				row := (idx%9-idx)/-9
-				return ((col%3-col)/-3) + (((row%3-row)/-3)*3)
+			squareIdx: func() int {
+				col := idx % 9
+				row := (idx%9 - idx) / -9
+				return ((col%3 - col) / -3) + (((row%3 - row) / -3) * 3)
 			}(),
 		}
 	}
 
-	return sk{
-		boxes: boxes,
+	(*s).boxes = boxes
+}
+
+func (s *sk) initOptions() {
+	rowsOp := make(map[int][]int)
+	colsOp := make(map[int][]int)
+	squaresOp := make(map[int][]int)
+
+	for i := 0; i < 9; i++ {
+		rowsOp[i] = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		colsOp[i] = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		squaresOp[i] = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	}
+
+	for _, box := range s.boxes {
+		if box.value != 0 {
+			rowsOp[box.rowIdx] = remove(rowsOp[box.rowIdx], box.value)
+			colsOp[box.colIdx] = remove(colsOp[box.colIdx], box.value)
+			squaresOp[box.squareIdx] = remove(squaresOp[box.squareIdx], box.value)
+		}
+	}
+
+	(*s).options.rows = rowsOp
+	(*s).options.cols = colsOp
+	(*s).options.squares = squaresOp
+}
+
+func remove(s []int, rV int) []int {
+	for i, v := range s {
+		if v == rV {
+			s = append(s[:i], s[i+1:]...)
+			break
+		}
+	}
+
+	return s
+}
+
+func contains(s []int, cV int) bool {
+	for _, v := range s {
+		if v == cV {
+			return true
+		}
+	}
+	return false
 }
--- main_test.go
+++ main_test.go
@@ -4,36 +4,96 @@ import (
 	"testing"
 )
 
-func TestCreateSk(t *testing.T) {
+func TestCreateSkInitBoxes(t *testing.T) {
 	sk := createSk("000000000035070840097302510003904100060000090009503700051608920026090450000000000")
 
-	boxesIdx := []int{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,80}
-
+	boxesIdx := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 80}
 
 	for idx, boxIdx := range boxesIdx {
 
-		expValue := []int{0,0,0,0,0,0,0,0,0,0,3,5,0,7,0,8,4,0,0,9,7,0}
+		expValue := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 5, 0, 7, 0, 8, 4, 0, 0, 9, 7, 0}
 
-		if  sk.boxes[boxIdx].value != expValue[idx] {
+		if sk.boxes[boxIdx].value != expValue[idx] {
 			t.Errorf("Expected box value=%v, got %v", expValue[idx], sk.boxes[boxIdx].value)
 		}
 
-		expRowIdx := []int{0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,8}
+		expRowIdx := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 8}
 
-		if  sk.boxes[boxIdx].rowIdx != expRowIdx[idx] {
+		if sk.boxes[boxIdx].rowIdx != expRowIdx[idx] {
 			t.Errorf("Expected box rowIdx=%v, got %v", expRowIdx[idx], sk.boxes[boxIdx].rowIdx)
 		}
 
-		expColIdx := []int{0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,8}
+		expColIdx := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 1, 2, 8}
 
-		if  sk.boxes[boxIdx].colIdx != expColIdx[idx] {
+		if sk.boxes[boxIdx].colIdx != expColIdx[idx] {
 			t.Errorf("Expected box colIdx=%v, got %v", expColIdx[idx], sk.boxes[boxIdx].colIdx)
 		}
 
-		expSquareIdx := []int{0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,8}
+		expSquareIdx := []int{0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 0, 0, 8}
 
-		if  sk.boxes[boxIdx].squareIdx != expSquareIdx[idx] {
+		if sk.boxes[boxIdx].squareIdx != expSquareIdx[idx] {
 			t.Errorf("Expected  box squareIdx=%v, got %v", expSquareIdx[idx], sk.boxes[boxIdx].squareIdx)
 		}
+	}
+}
+
+func TestCreateSkInitOptions(t *testing.T) {
+	sk := createSk("000000000035070840097302510003904100060000090009503700051608920026090450000000000")
+
+	if !contains(sk.options.rows[6], 7) ||
+		!contains(sk.options.rows[6], 4) ||
+		!contains(sk.options.rows[6], 3) {
+		t.Errorf(
+			"Expected row options contains %v, got false. Options are: %v",
+			"7 and 4 and 3",
+			sk.options.rows[7],
+		)
+	}
+
+	if len(sk.options.rows[6]) != 3 {
+		t.Errorf(
+			"Expected row options length equal to %v, got %v",
+			3,
+			len(sk.options.rows[7]),
+		)
+	}
+
+	if !contains(sk.options.cols[1], 8) ||
+		!contains(sk.options.cols[1], 7) ||
+		!contains(sk.options.cols[1], 4) ||
+		!contains(sk.options.cols[1], 1) {
+		t.Errorf(
+			"Expected columns options contains %v, got false. Options are: %v",
+			"7 and 4 and 1",
+			sk.options.cols[1],
+		)
+	}
+
+	if len(sk.options.cols[1]) != 4 {
+		t.Errorf(
+			"Expected columns options length equal to %v, got %v",
+			4,
+			len(sk.options.cols[1]),
+		)
+	}
+
+	if !contains(sk.options.squares[6], 3) ||
+		!contains(sk.options.squares[6], 4) ||
+		!contains(sk.options.squares[6], 7) ||
+		!contains(sk.options.squares[6], 8) ||
+		!contains(sk.options.squares[6], 9) {
+		t.Errorf(
+			"Expected squares options contains %v, got false. Options are: %v",
+			"3, 4, 7, 8 and 9",
+			sk.options.squares[6],
+		)
+	}
+
+	if len(sk.options.squares[6]) != 5 {
+		t.Errorf(
+			"Expected squares options length equal to %v, got %v",
+			5,
+			len(sk.options.squares[6]),
+		)
 	}
 }
